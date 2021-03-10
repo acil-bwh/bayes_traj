932e4ec249445bebfe6ba33d8edcea20ce1b366c
James Ross <jross@bwh.harvard.edu>
2021-03-10 14:30:13 -0500

    Incrementing version -- now 0.0.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='bayes_traj',
-    version='0.0.0',
+    version='0.0.1',
     description='bayes_traj',
     long_description=long_description,
     long_description_content_type='text/markdown',
