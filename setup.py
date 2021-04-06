e3dd24178e248bf8ad7823aba6ef05e721adc269
James Ross <jross@bwh.harvard.edu>
2021-04-05 21:08:45 -0400

    Incrementing to version 0.0.8

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ packages.append('bayes_traj')
 
 setup(
     name='bayes_traj',
-    version='0.0.7',
+    version='0.0.8',
     description='bayes_traj',
     long_description=long_description,
     long_description_content_type='text/markdown',
