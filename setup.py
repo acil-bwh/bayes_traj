988822c95e135eec8a949445a6477512c4777bc6
James Ross <jross@bwh.harvard.edu>
2024-08-20 13:56:01 -0400

    Incrementing version to 1.0.0. Random effects now implemented

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ packages.append('bayes_traj')
 
 setup(
     name='bayes_traj',
-    version='0.2.7',
+    version='1.0.0',
     description='bayes_traj',
     long_description=long_description,
     long_description_content_type='text/markdown',
